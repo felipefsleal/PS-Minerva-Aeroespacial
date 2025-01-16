3570c8262be3a2ddec5df9dc8a2443305e863b7e
felipefsleal <113049811+felipefsleal@users.noreply.github.com>
2025-01-16 02:00:45 +0000

    melhorando o código de bits

--- Atividades/Manipulacao_bits.c
+++ Atividades/Manipulacao_bits.c
@@ -9,33 +9,38 @@ void print_binario(unsigned char num) {
 
 int main() {
     unsigned char num1 = 0b10110011;  
-    unsigned char num2 = 0b11010101; 
+    unsigned char num2 = 0b11010101;  
 
     printf("Número Binário 1: ");
-    printBinary(num1);
+    print_binario(num1);
     printf("Número Binário 2: ");
-    printBinary(num2);
+    print_binario(num2);
 
-    unsigned char comparison = num1 ^ num2;
-    printf("Comparação bit a bit: ");
-    printBinary(comparison);
+    unsigned char comparison = 0;
+    for (int i = 0; i < 8; i++) {
+        unsigned char bit1 = (num1 >> i) & 1;
+        unsigned char bit2 = (num2 >> i) & 1;
+        comparison |= ((bit1 == bit2) << i);
+    }
+    printf("\nComparação bit a bit: ");
+    print_binario(comparison);
 
     unsigned char leftShift = num1 << 1;
     printf("Deslocamento à esquerda: ");
-    printBinary(leftShift);
+    print_binario(leftShift);
 
     unsigned char rightShift = num1 >> 1;
     printf("Deslocamento à direita: ");
-    printBinary(rightShift);
+    print_binario(rightShift);
 
     if (num1 & 1) {  
-        printf("O bit menos significativo é 1.\n");
+        printf("\nO bit menos significativo é 1.\n");
         unsigned char inverted = ~num1;
         printf("Invertendo todos os bits...\n");
         printf("Número invertido: ");
-        printBinary(inverted);
+        print_binario(inverted);
     } else {
-        printf("O bit menos significativo está inativo.\n");
+        printf("\nO bit menos significativo está inativo.\n");
     }
 
     return 0;
